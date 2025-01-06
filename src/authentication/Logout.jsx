3bfaf65dad7c23ecd299c01d5684e98a079be3e4
DevCHIDI <chidimathayas@gmail.com>
2025-01-06 14:13:28 +0100

    Logout updated

--- src/authentication/Logout.jsx
+++ src/authentication/Logout.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ButtonIcon from "../ui/ButtonIcon";
 import { HiArrowRightEndOnRectangle } from "react-icons/hi2";
+import { logout } from "../services/apiAuth";
+import useLogout from "./useLogout";
+import SpinnerMini from "../ui/SpinnerMini";
 
 const Logout = () => {
+	const { logout, isLoading } = useLogout();
+
 	return (
-		<ButtonIcon>
-			<HiArrowRightEndOnRectangle />
+		<ButtonIcon disabled={isLoading} onClick={logout}>
+			{!isLoading ? <HiArrowRightEndOnRectangle /> : <SpinnerMini />}
 		</ButtonIcon>
 	);
 };
