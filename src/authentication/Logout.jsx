7e2a447110545375d35f1e140a035ef8f52593ec
DevCHIDI <chidimathayas@gmail.com>
2025-01-06 10:42:46 +0100

    Logout Added

---
+++ src/authentication/Logout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ButtonIcon from "../ui/ButtonIcon";
+import { HiArrowRightEndOnRectangle } from "react-icons/hi2";
+
+const Logout = () => {
+	return (
+		<ButtonIcon>
+			<HiArrowRightEndOnRectangle />
+		</ButtonIcon>
+	);
+};
+
+export default Logout;
