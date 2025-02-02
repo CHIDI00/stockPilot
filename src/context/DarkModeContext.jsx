a768a577df74b194c42730403a0caa4d222de232
DevCHIDI <chidimathayas@gmail.com>
2025-02-02 23:56:24 +0100

    Updated

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -1,8 +1,28 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 import { useLocalStorageState } from "../hooks/useLocalStorageState";
 
 const DarkModeContext = createContext();
 
 function DarkModProvider({ children }) {
-	const [isSarkMode, setIsDarkMode] = useLocalStorageState();
+	const [isSarkMode, setIsDarkMode] = useLocalStorageState(false, "isDarkMode");
+
+	function toggleDarkMode() {
+		setIsDarkMode((isDark) => !isDark);
+	}
+
+	return (
+		<DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+			{children}
+		</DarkModeContext.Provider>
+	);
 }
+
+const useDarkMode = () => {
+	const context = useContextt(DarkModeContext);
+
+	if (context === undefined)
+		throw new Error("DarkModeContext was used outside the DarkModeProvider");
+	return context;
+};
+
+export { DarkModProvider, useDarkMode };
