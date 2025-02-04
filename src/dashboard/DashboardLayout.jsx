2923d5b192c1e8fe9de30a8ffb9fa710fd160289
DevCHIDI <chidimathayas@gmail.com>
2025-02-04 14:01:57 +0100

    Dashboard

--- src/dashboard/DashboardLayout.jsx
+++ src/dashboard/DashboardLayout.jsx
@@ -1,8 +1,47 @@
 import styled from "styled-components";
 
 const StyledDashboardLayout = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-template-rows: auto 34rem auto;
-  gap: 2.4rem;
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+	gap: 2rem;
 `;
+
+const DashboardContentContainer = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	width: 100%;
+	gap: 2rem;
+`;
+// const StyledDashboardLayout = styled.div`
+//   display: grid;
+//   grid-template-columns: 1fr 1fr 1fr 1fr;
+//   grid-template-rows: auto 34rem auto;
+//   gap: 2.4rem;
+// `;
+
+const DashboardLayout = () => {
+	return (
+		<StyledDashboardLayout>
+			<DashboardContentContainer>
+				<div>sales Overview</div>
+				<div>Inventory Summary</div>
+			</DashboardContentContainer>
+			<DashboardContentContainer>
+				<div>sales Overview</div>
+				<div>Inventory Summary</div>
+			</DashboardContentContainer>
+			<DashboardContentContainer>
+				<div>sales Overview</div>
+				<div>Inventory Summary</div>
+			</DashboardContentContainer>
+			<DashboardContentContainer>
+				<div>sales Overview</div>
+				<div>Inventory Summary</div>
+			</DashboardContentContainer>
+		</StyledDashboardLayout>
+	);
+};
+
+export default DashboardLayout;
