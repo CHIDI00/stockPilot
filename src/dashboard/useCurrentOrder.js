47b02f4368e9f408a4df96546a9d5c4bf2993486
DevCHIDI <chidimathayas@gmail.com>
2025-02-16 23:30:04 +0100

    ``` No code changes made ```

--- src/dashboard/useCurrentOrder.js
+++ src/dashboard/useCurrentOrder.js
@@ -1,4 +1,7 @@
 import { useSearchParams } from "react-router-dom";
+import { subDays } from "date-fns";
+import { useQuery } from "@tanstack/react-query";
+import { getOrdersAfterDate } from "../services/apiOrders";
 
 export const userCurrentOrder = () => {
 	const [searchParams] = useSearchParams();
@@ -6,5 +9,12 @@ export const userCurrentOrder = () => {
 	const numDays = !searchParams.get("last")
 		? 7
 		: Number(searchParams.get("Last"));
-	const queryDate = sub;
+	const queryDate = subDays(new Date(), numDays).toISOString();
+
+	const { data: orders, isloading } = useQuery({
+		queryFn: getOrdersAfterDate(queryDate),
+		queryKey: ["orders", `last-${numDays}`],
+	});
+
+	return { isLoading, orders };
 };
